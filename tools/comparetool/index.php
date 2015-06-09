5a3da4ec9426965d65a65a62e004459844be011a
heyvin <heyvin.nouhengy@gmail.com>
2015-06-09 02:32:43 +0000

    Modified CompareTool

--- tools/comparetool/index.php
+++ tools/comparetool/index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<title>Heyvin's Tools</title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 	<script src="/js/jquery-1.11.2.min.js"></script>
 	<link rel="stylesheet" href="/css/materialize.css">
 	<!-- CodeMirror Dependencies -->
@@ -10,7 +11,7 @@
 	<script src="/tools/codemirror/mode/clike/clike.js"></script>
 	<style>
 		.control-wrapper {
-			position: relative;
+			position: absolute;
 			text-align: right;
 			margin-top: 25px;
 		}
@@ -25,12 +26,14 @@
 		</header>
 		<form id="myForm" method="post">
 			<div class="row">
-				<div class="col s10 control-wrapper pinned">
+				<div class="col s2 offset-s8 control-wrapper pinned">
 					<div class="button-container">
 						<br>
 						<a href="#" id="btnSubmit" class="waves-effect waves-light btn-large"><i class="mdi-action-exit-to-app left"></i>COMPARE</a>
 						<br><br>
 						<a href="#" id="btnClear" class="waves-effect waves-light btn-large red lighten-1"><i class="mdi-navigation-refresh left"></i>CLEAR</a>
+						<br><br>
+						<a href="#" id="btnHow" class="waves-effect waves-light btn-large orange lighten-1"><i class="mdi-action-help left"></i>WTF?!?</a>
 					</div>
 				</div>
 				<div class="col s8">
@@ -49,6 +52,23 @@
 				</div>
 			</div>
 		</form>
+		<div id="mdlHow" class="modal modal-fixed-footer">
+			<div class="modal-content">
+				<h4>Đây Là Cái Nồi Gì</h4>
+				<p>Lỡ có viết cái doQuery tới hơn trăm cái biến mà nó báo lỗi thì không biết đường nào mà lần ra biến không hợp kiểu. Vì lí do làm biếng nói chung nên tool này được tạo ra.</p>
+				<h4>Cách Sử Dụng</h4>
+				<p><b>B1: Lấy thông báo lỗi từ method.</b></p>
+				<p><b>Eg:</b> The method doQuery(int, int, String, String, String, String)
+				</p>
+				<p><b>B2: Mở file SQL Java ra tìm chỗ khai báo biến rồi dán vô đây.</b></p>
+				<p><b>Eg:</b> int idoden_no, int idomeisai_no, String nendo, String can_kbn, String juha_no1, String juha_no2)
+				</p>
+				<p><b>B3: Nhấn COMPARE để sướng.</b></p>
+			</div>
+			<div class="modal-footer">
+				<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">OK</a>
+			</div>
+		</div>
 		<script type="text/javascript">
 		
 			// CodeMirror
@@ -126,6 +146,10 @@
 					cmSQL.setValue("");
 					cmConflict.setValue("");
 					
+				});
+				
+				$("#btnHow").click(function() {
+					$('#mdlHow').openModal();	
 				});
 
 			});
